3cb2ef57e3c0bd3a23a24c66fc2ca3f88f73e288
lam1st <starodubcevkiril90@email.com>
2026-03-14 13:35:19 +0300

    Initial commit

--- colors.js
+++ colors.js
@@ -1,8 +1,8 @@
 function generateColor() {
-    // Символы для HEX цвета
+   
     let symbols = ["0",'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
     
-    // Начало цвета
+    
     let color = "#"
     
     
@@ -14,9 +14,9 @@ function generateColor() {
 
    }
     
-    // Изменить фон
+   
     document.body.style.backgroundColor = color
     
-    // Показать код
+   
     document.getElementById('colorCode').textContent = color
 }
